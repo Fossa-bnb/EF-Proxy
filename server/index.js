7763863689f12d3b634c68e411875d9c001b1eac
EvaFro <frolli.erin@gmail.com>
2018-03-08 17:40:10 -0800

    fill in basic server script

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const App = express();
+const PORT = 4000 // will add evn path also
+
+// will change this to be the correct folder
+App.use(express.static(`${__dirname}/../client`));
+
+App.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
