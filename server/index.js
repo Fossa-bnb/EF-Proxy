6039ada54ca7b44a6e05635330b51b7f2879b4b7
EvaFro <frolli.erin@gmail.com>
2018-03-08 17:47:38 -0800

    fix eslint errors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const App = express();
-const PORT = 4000 // will add evn path also
+const PORT = 4000; // will add evn path also
 
 // will change this to be the correct folder
 App.use(express.static(`${__dirname}/../client`));
